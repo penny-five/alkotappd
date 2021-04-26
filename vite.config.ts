a6ca95a78a33d036f0226a50d99899914b9c55b4
penny-five <lehton83@gmail.com>
2021-04-26 18:06:53 +0300

    fix vite dev server api proxy url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5001/alkotappd/us-central1'
+        target: 'http://127.0.0.1:5001/alkotappd/us-central1/api'
       }
     }
   }
